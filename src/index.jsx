365dfbc8a06b218af240dc5dc1bd88e854c51c8b
Gaurav Srivastava <gauravjss@gmail.com>
2018-03-13 18:27:35 -0500

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDOM = require("react-dom");
+var home_1 = require("./components/home");
+ReactDOM.render(<home_1.Home name={'Gaurav'} age={33}/>, document.getElementById("app"));
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {Home} from "./components/home";
+
+ReactDOM.render(<Home name={'Gaurav'} age={33}/>,document.getElementById("app"));
